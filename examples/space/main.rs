21f2e50e8ff9f763befcbe6c0b71d202e4ec0607
Trisfald <trisfald@gmail.com>
2020-02-15 23:38:21 +0000

    minor improvements to the space example

--- examples/space/main.rs
+++ examples/space/main.rs
@@ -18,11 +18,13 @@ fn main() {
     // Create a server to manage the battle.
     let battle = Battle::builder(CustomRules::new()).build();
     let mut server = Server::builder(battle).build();
-    // Set the space model to be a 2D battleground of squares.
+    // Set the space model to be a 2D battlefield of squares.
     ResetSpace::trigger(&mut server)
         .seed(BattlefieldSeed::TwoDimensions)
         .fire()
         .unwrap();
+    // Display the space model.
+    println!("Battlefield:\n{}", server.battle().space().model());        
     // Spawn three creatures.
     println!("Spawning three creatures...");
     CreateTeam::trigger(&mut server, TEAM_ID).fire().unwrap();
@@ -60,14 +62,16 @@ fn main() {
     .fire()
     .unwrap();
     assert_eq!(server.battle().entities().entities().count(), 2);
+    println!("");
     // Display the space model and the creatures. Some of them died!
     println!("Battlefield:\n{}", server.battle().space().model());
-    // Now drop completely reset the space, dropping one dimension.
+    // Now completely reset the space model, dropping one dimension.
     println!("Removing the y-axis!");
     ResetSpace::trigger(&mut server)
         .seed(BattlefieldSeed::OneDimension)
         .fire()
         .unwrap();
+    println!("");
     // Display the space model and the creatures. Their positions have been adapted!
     println!("Battlefield:\n{}", server.battle().space().model());
 }
